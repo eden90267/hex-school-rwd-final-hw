e070bd81ddae9fe2e3ef03f4f2d06c3183938aa3
eden90267 <eden90267@gmail.com>
2017-02-25 04:29:24 +0800

    impl cart page

--- js/all.js
+++ js/all.js
@@ -2,3 +2,7 @@ $('.menu-hamburger').on('click',  function(e){
     e.preventDefault();
     $('body').toggleClass('menu-show');
 });
+
+$('.cart-content-item ul li button').hover(function() {
+	$(this).toggleClass('btn-hover');
+})
